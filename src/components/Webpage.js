b373c820e14bed3c2394b098e624aca0ef4783ec
Jason Vance <jbvance@gmail.com>
2017-12-23 16:27:44 +0000

    automatically backed up by learn

--- src/components/Webpage.js
+++ src/components/Webpage.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import FillerText from './FillerText';
 
-class Webpage extends Compnonent {
-  render () {
+class Webpage extends Component {
+  render() {
     return (
       <div>
-        <title>{"The world's coolest webpage"}</title>
-        <FillerText/>
-        <FillerText/>
+        <title>
+          {"The world's coolest webpage"}
+        </title>
+        <FillerText />
+        <FillerText />
       </div>
     );
   }
