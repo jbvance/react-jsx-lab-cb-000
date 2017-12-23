876d2edd978c233c4b44625277d68260ff9bb56a
Jason Vance <jbvance@gmail.com>
2017-12-23 16:19:01 +0000

    automatically backed up by learn

---
+++ src/components/RegistrationForm.js
@@ -0,0 +1,13 @@
+import React { Component } from 'react';
+
+class RegistrationForm extends Compnonent {
+  render () {
+    return (
+      <form>
+        <input type="text">
+        <input type="password">
+        <input type="submit" value="submit">
+      </form>
+    );
+  }
+}
